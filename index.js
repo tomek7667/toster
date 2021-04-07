b769a2167d33ffcc6e5bc2a3fbfe7fdc5262a782
tomek7667 <38402753+tomek7667@users.noreply.github.com>
2021-04-07 06:14:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+    console.log("req:", req);
+    res.render('pages/index')
+});
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
